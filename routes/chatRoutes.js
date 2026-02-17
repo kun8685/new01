63844163ce5d4ea5357665e6b1b4f3b16fd9737c
kun8685 <p8685164@gmail.com>
2026-02-17 09:44:29 +0530

    refactor: cleanup duplicate backend files from root directory

--- routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChatHistory, getAllChats } = require('../controllers/chatController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.route('/').get(protect, admin, getAllChats);
-router.route('/:userId').get(protect, getChatHistory);
-
-module.exports = router;
